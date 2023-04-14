ae13785f61dd858f7d69f468a234512dffca0242
Jennyads <jennycristina.1998@gmail.com>
2023-04-14 17:30:40 -0300

    add config

---
+++ SocialNetwork/api/src/banco/banco.go
@@ -0,0 +1,19 @@
+package banco
+
+import (
+	"api/src/config"
+	"database/sql"
+)
+
+//Conectar abre a conexão com o banco de dados e a retorna
+func Conectar() (*sql.DB, error){
+	db, erro := sql.Open("mysql", config.StringConexaoBanco)
+	if erro != nil {
+		return nil, erro
+	}
+	if erro = db.Ping(); erro != nil {
+		db.Close()
+		return nil, erro
+	}
+	return db, nil
+}
