bc32b97b9279c58e5c0ecdfff8e6c36f77469d39
Jennyads <jennycristina.1998@gmail.com>
2023-04-17 14:08:11 -0300

    create error responses

--- SocialNetwork/api/src/config/config.go
+++ SocialNetwork/api/src/config/config.go
@@ -30,9 +30,9 @@ func Carregar(){
 		Porta = 9000
 	}
 	StringConexaoBanco = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
-	os.Getenv("DB_USUARIO"),
-	os.Getenv("DB_SENHA"),
-	os.Getenv("DB_NOME"),
+		os.Getenv("DB_USUARIO"),
+		os.Getenv("DB_SENHA"),
+		os.Getenv("DB_NOME"),
 	)
 	
 
--- SocialNetwork/api/src/controllers/usuarios.go
+++ SocialNetwork/api/src/controllers/usuarios.go
@@ -1,10 +1,48 @@
 package controllers
 
-import "net/http"
+import (
+	"api/src/banco"
+	"api/src/modelos"
+	"api/src/repositorios"
+	"api/src/respostas"
+	"encoding/json"
+	"io"
+	"net/http"
+)
 
 //CriarUsuario insere um usuário no banco de dados
-func CriarUsuario(w http.ResponseWriter, R *http.Request){
-	w.Write([]byte("Criando usuário!"))
+func CriarUsuario(w http.ResponseWriter, r *http.Request){
+	corpoRequest, erro := io.ReadAll(r.Body)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+		
+	}
+
+	var usuario modelos.Usuario
+	if erro = json.Unmarshal(corpoRequest, &usuario); erro != nil {
+		respostas.Erro(w, http.StatusBadRequest, erro)
+		return
+	}
+
+	db, erro := banco.Conectar()
+	if erro != nil {
+		respostas.Erro(w, http.StatusInternalServerError, erro)
+		return
+	}
+
+	defer db.Close()
+
+	repositorio := repositorios.NovoRepositorioDeUsuarios(db)
+	usuario.ID, erro = repositorio.Criar(usuario)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnprocessableEntity, erro)
+		return
+	}
+	respostas.JSON(w, http.StatusCreated, usuario)
+
+	
+
 }
 //BuscarUsuarios busca todos os usuários salvos no banco de dados
 func BuscarUsuarios(w http.ResponseWriter, R *http.Request){
