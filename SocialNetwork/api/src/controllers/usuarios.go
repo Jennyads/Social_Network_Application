192e89ab589132480f98430fee47d80260761438
Jennyads <jennycristina.1998@gmail.com>
2023-04-24 13:26:07 -0300

    add token validation

--- SocialNetwork/api/src/controllers/usuarios.go
+++ SocialNetwork/api/src/controllers/usuarios.go
@@ -159,6 +159,16 @@ func DeletarUsuario(w http.ResponseWriter, r *http.Request){
 		respostas.Erro(w, http.StatusBadRequest, erro)
 		return
 	}
+	usuarioIDNoToken, erro := autenticacao.ExtrairUsuarioID(r)
+	if erro != nil {
+		respostas.Erro(w, http.StatusUnauthorized, erro)
+		return
+	}
+
+	if usuarioID != usuarioIDNoToken {
+		respostas.Erro(w, http.StatusForbidden, errors.New("não é possível deletar um usuário que não seja o seu"))
+		return
+	}
 
 	db, erro := banco.Conectar()
 	if erro != nil {
