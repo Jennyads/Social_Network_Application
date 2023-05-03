2a7121cea4808b0593cda3b7d841e9a91bb4748a
Jennyads <jennycristina.1998@gmail.com>
2023-05-03 18:20:06 -0300

    add jquery

---
+++ SocialNetwork/webapp/src/controllers/paginas.go
@@ -0,0 +1,17 @@
+package controllers
+
+import (
+	"net/http"
+	"webapp/src/utils"
+)
+
+// CarregarTelaDeLogin vai renderizar a tela de login
+func CarregarTelaDeLogin(w http.ResponseWriter, r *http.Request) {
+	utils.ExecutarTemplate(w, "login.html", nil)
+
+}
+
+
+func CarregarPaginaDeCadastroDeUsuario(w http.ResponseWriter, r *http.Request) {
+	utils.ExecutarTemplate(w, "cadastro.html", nil)
+}
