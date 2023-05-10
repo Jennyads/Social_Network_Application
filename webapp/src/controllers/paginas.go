02aba8e41a498df6c1c3c9e7965f4ceca54fcf9b
Jennyads <jennycristina.1998@gmail.com>
2023-05-10 19:17:56 -0300

    add icons

--- webapp/src/controllers/paginas.go
+++ webapp/src/controllers/paginas.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 	"webapp/src/config"
+	"webapp/src/cookies"
 	"webapp/src/modelos"
 	"webapp/src/requisicoes"
 	"webapp/src/respostas"
@@ -13,21 +15,27 @@ import (
 
 // CarregarTelaDeLogin vai renderizar a tela de login
 func CarregarTelaDeLogin(w http.ResponseWriter, r *http.Request) {
-	utils.ExecutarTemplate(w, "login.html", nil)
+	cookie, _ := cookies.Ler(r)
 
+	if cookie["token"] != "" {
+		http.Redirect(w, r, "/home", 302)
+		return
+	}
+
+	utils.ExecutarTemplate(w, "login.html", nil)
 }
 
-//CarregarPaginaDeCadastroDeUsuario vai carregar a página de cadastro de usuário
+// CarregarPaginaDeCadastroDeUsuario carrega a página de cadastro de usuário
 func CarregarPaginaDeCadastroDeUsuario(w http.ResponseWriter, r *http.Request) {
 	utils.ExecutarTemplate(w, "cadastro.html", nil)
 }
 
-//CarregarPaginaPrincipal carrega a página principal com as publicações
+// CarregarPaginaPrincipal carrega a página principal com as publicações
 func CarregarPaginaPrincipal(w http.ResponseWriter, r *http.Request) {
-	url := fmt.Sprintf("%spublicacoes", config.APIURL)
+	url := fmt.Sprintf("%s/publicacoes", config.APIURL)
 	response, erro := requisicoes.FazerRequisicaoComAutenticacao(r, http.MethodGet, url, nil)
 	if erro != nil {
-		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro : erro.Error()})
+		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: erro.Error()})
 		return
 	}
 	defer response.Body.Close()
@@ -39,10 +47,18 @@ func CarregarPaginaPrincipal(w http.ResponseWriter, r *http.Request) {
 
 	var publicacoes []modelos.Publicacao
 	if erro = json.NewDecoder(response.Body).Decode(&publicacoes); erro != nil {
-		respostas.JSON(w, http.StatusUnprocessableEntity, respostas.ErroAPI{Erro : erro.Error()})
+		respostas.JSON(w, http.StatusUnprocessableEntity, respostas.ErroAPI{Erro: erro.Error()})
 		return
-
 	}
-	
-	utils.ExecutarTemplate(w, "home.html", publicacoes)
+
+	cookie, _ := cookies.Ler(r)
+	usuarioID, _ := strconv.ParseUint(cookie["id"], 10, 64)
+
+	utils.ExecutarTemplate(w, "home.html", struct {
+		Publicacoes []modelos.Publicacao
+		UsuarioID   uint64
+	}{
+		Publicacoes: publicacoes,
+		UsuarioID:   usuarioID,
+	})
 }
